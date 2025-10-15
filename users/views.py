8433f4a0f9099df3fdfeff13333a3672b41e08da
Hafz-technology <khalilhafz@gmail.com>
2025-10-15 09:05:20 +0300

    setup users app part

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegisterSerializer
+from .models import CustomUser
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegisterSerializer
+    
+    
